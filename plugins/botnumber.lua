93e1993e15f9a997d45f6f942bccb430f771483d
abbasgh12345 <abbasgh91@gmail.com>
2016-02-29 22:44:25 +0330

    Update botnumber.lua

--- plugins/botnumber.lua
+++ plugins/botnumber.lua
@@ -1,12 +1,12 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+13603353378", "NOD32", "BOT", ok_cb, false)
+send_contact(get_receiver(msg), "+639080540666", "E X T R E M E🌏", "", ok_cb, false)
 end
 
 return {
 patterns = {
-"^!botnumber$"
+"^!bot$"
 
 },
 run = run
